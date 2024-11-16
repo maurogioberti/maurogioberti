82c8706ec5c78afbdbda4c21f6cea1f4a977993a
Mauro Gioberti <maurol.gioberti@gmail.com>
2024-11-16 01:06:16 +0100

    Update Deploy Settings 🚀

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enable static export for GitHub Pages
-    images: {
-      unoptimized: true, // Disable image optimization for GitHub Pages
-    },
-    basePath: '/maurogioberti', // Set base path to your repository name
-    assetPrefix: '/maurogioberti/', // Prefix assets with your repository name
-  };
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
